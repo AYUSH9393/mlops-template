66bca801e64a560ef1afc8990fa54e5be3596a27
AYUSH9393 <ap207789@gmail.com>
2025-11-23 22:00:54 +0530

    add logger file

---
+++ app/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+def get_logger(name: str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '{"timestamp": "%(asctime)s", ' \
+        '"level": "%(levelname)s", ' \
+        '"event": "%(message)s"}'
+    )
+    handler.setFormatter(formatter)
+
+    if not logger.handlers:
+        logger.addHandler(handler)
+
+    return logger
